0fb3eddedb8015833b2c78c870c66c7cfea43828
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-09-11 21:04:23 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+@Component({ //Decorador
+  selector: 'app-root', //llamar los componentes
+  templateUrl: './app.component.html', //invocar el archivo html
+  standalone: false, //
+  styleUrl: './app.component.css' //hace referencia a la hoja de estilos
 })
+
+
 export class AppComponent {
-  title = 'basesAngular';
+  title = 'Alex Eduardo Garcia Ramirez';
+
+  //Funcion
+  duplicarNumero(valor:number):number{
+  return valor*2;
 }
+
+//Objeto
+anime={
+  titulo:"My dress up darling",
+  anio: 2025,
+  genero:"Romance/Comedia"
+}
+
+
+}
+
+
