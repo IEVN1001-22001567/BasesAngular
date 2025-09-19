15b9d325b91ff096fcce854654eab993a75b16c2
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-09-18 21:04:09 -0600

    boton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonajesListComponent } from './personajes/personajes-list/personajes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonajesListComponent
   ],
   imports: [
     BrowserModule,
